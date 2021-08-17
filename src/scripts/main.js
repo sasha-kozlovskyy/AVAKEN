c42dcb71e355de704ce5d7eb25300b2fdceaa8d7
Sasha Kozlovskyy <alex33507@gmail.com>
2021-08-17 16:11:23 +0300

    Start

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const h = document.querySelector('.nav__menu');
+const n = document.querySelector('.nav');
+
+h.addEventListener('click', () => {
+  n.classList.toggle('nav--activ');
+});
