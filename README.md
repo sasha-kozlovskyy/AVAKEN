5e8bb1fe238309168b0aa625a9caab2e4e562084
Sasha Kozlovskyy <alex33507@gmail.com>
2021-08-19 09:33:45 +0300

    80%

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 - [Design](https://www.figma.com/file/dFbqcI4YVgXAYNV0jNU9oq/Test-work-for-developers?node-id=0%3A71)
 - [DEMO LINK](https://sasha-kozlovskyy.github.io/AVAKEN/)
+
+Requirements
+
+1 JS Sticky header (state when scrolled: https://www.dropbox.com/sh/odxu6qft486266z/AADT6pDF1MqMP_MN_V3cxgABa/screens?dl=0&preview=01-js-sticky-header.png&subfolder_nav_tracking=1)
+2 JS Slider (https://www.dropbox.com/sh/odxu6qft486266z/AADT6pDF1MqMP_MN_V3cxgABa/screens?dl=0&preview=02-js-slider.png&subfolder_nav_tracking=1) – use this plugin: https://swiperjs.com/
+3 JS Burger menu (https://www.dropbox.com/sh/odxu6qft486266z/AADT6pDF1MqMP_MN_V3cxgABa/screens?dl=0&preview=03-js-burger-menu.png&subfolder_nav_tracking=1)
+4 Use BEM key concepts & naming convention during development (https://en.bem.info/methodology/)
+5 Use Sass or SCSS
+6 DO NOT use any kind of FE frameworks such as: Bootstrap, Materialize, etc.
+7 Responsive (design included)
+8 Use jQuery (latest stable version)
+9 Project should look the same on all modern browsers (crossbrowser compatible)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,40 @@
+/* eslint-disable no-undef */
 'use strict';
 
-const h = document.querySelector('.nav__menu');
+const h = document.querySelector('.menu');
 const n = document.querySelector('.nav');
 
 h.addEventListener('click', () => {
   n.classList.toggle('nav--activ');
 });
+
+// eslint-disable-next-line no-undef
+// eslint-disable-next-line no-unused-vars
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-right',
+    prevEl: '.swiper-button-left',
+  },
+});
+
+const header = document.querySelector('.header');
+const link = document.querySelectorAll('.nav__link');
+
+const origOffsetY = 100;
+
+function onScroll(e) {
+  window.scrollY >= origOffsetY
+    ? header.classList.add('sticky', 'backgroud')
+    : header.classList.remove('sticky', 'backgroud');
+}
+
+function onScrollColor(e) {
+  window.scrollY >= origOffsetY
+    ? link.map(x => x.classList.add('color'))
+    : link.map(x => x.classList.remove('color'));
+}
+
+document.addEventListener('scroll', onScroll);
+document.addEventListener('scroll', onScrollColor);
